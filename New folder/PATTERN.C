86370d208b933ff250b5370a3c8cc6fbff0acd8e
Shivansh pathak <92042011+pshivansh14@users.noreply.github.com>
2022-06-03 00:51:04 +0530

    .

--- New folder/PATTERN.C
+++ New folder/PATTERN.C
@@ -5,12 +5,7 @@ int main()
     {
         for(int j=1;j<=i;j++)
         {
-            printf(" ");
-
-        for(int k=0;k<j;k++)
-        {
             printf("*");
-        }
         }
         printf("\n");
     }
